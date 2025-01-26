65db9f2c0ba89a16a12aa631fd37a30b41fef017
Davidtanghengkai <S10268400@connect.np.edu.sg>
2025-01-26 20:10:12 +0800

    commit

--- PRG2 ASSG/Program.cs
+++ PRG2 ASSG/Program.cs
@@ -1,11 +1,10 @@
-﻿
+﻿using PRG2_ASSG;
 
-
-using PRG2_ASSG;
 
 static void loadfiles()
 {
-     List<Airline> Airplanelist = new List<Airline>();
+    Terminal terminal = new Terminal();
+    List<Airline> Airplanelist = new List<Airline>();
     using (StreamReader sr = new StreamReader("airplanes.csv"))
     {
         string s = sr.ReadLine();
@@ -13,8 +12,10 @@ static void loadfiles()
         while ((s = sr.ReadLine()) != null)
         {
             string[] airplane = s.Split(',');
-            
+            Airline a = new Airline(airplane[0], airplane[1]);
+            terminal.AddAirline(a);
             Airplanelist.Add(new Airline(airplane[0], airplane[1]));
+            
         }
         
     }
@@ -27,6 +28,8 @@ static void loadfiles()
         while ((x = sr.ReadLine()) != null)
         {
             string[] gateperm = x.Split(',');
+            BoardingGate b = new BoardingGate(gateperm[0], Convert.ToBoolean(gateperm[1]), Convert.ToBoolean(gateperm[2]), Convert.ToBoolean(gateperm[3]));
+            terminal.AddBoardingGate(b);
             
             BoardingGateList.Add(new BoardingGate(gateperm[0], Convert.ToBoolean(gateperm[1]), Convert.ToBoolean(gateperm[2]), Convert.ToBoolean(gateperm[3])));
         }
